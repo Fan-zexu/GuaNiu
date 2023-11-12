e930e9b2676599da61feb08e21a2d80d4fd300c8
Fan-zexu <1152015013@qq.com>
2023-11-12 21:22:31 +0800

    react-router test code

--- react/react-router-demo/src/index.js
+++ react/react-router-demo/src/index.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Link, Outlet  } from 'react-router-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Aaa() {
+  return <div>
+    <p>aaa</p>
+    <Link to={'/bbb/111'}>to bbb</Link>
+    <br/>
+    <Link to={'/ccc'}>to ccc</Link>
+    <br/>
+    <Outlet/>
+  </div>;
+}
+
+function Bbb() {
+  return 'bbb';
+}
+
+function Ccc() {
+  return 'ccc';
+}
+
+function ErrorPage() {
+  return 'error';
+}
+
+const routes = [
+  {
+    path: "/",
+    element: <Aaa/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "bbb/:id",
+        element: <Bbb />,
+      },
+      {
+        path: "ccc",
+        element: <Ccc />,
+      }    
+    ],
+  }
+];
+
+const router = createBrowserRouter(routes);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
