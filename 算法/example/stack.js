7b0381296cfc299f3fe6b733726e7120a2fc3d35
Fan-zexu <1152015013@qq.com>
2024-01-29 19:43:10 +0800

    10进制转2进制

---
+++ 算法/example/stack.js
@@ -0,0 +1,28 @@
+// 时间复杂度 O(n) n为二进制的长度
+// 空间复杂度 O(n) n为二进制的长度
+const dec2bin = (dec) => {
+  // 创建一个字符串
+  let res = "";
+
+  // 创建一个栈
+  let stack = [];
+
+  // 遍历数字 如果大于0 就可以继续转换2进制
+  while (dec > 0) {
+    // 将数字的余数入栈
+    stack.push(dec % 2);
+
+    // 除以2
+    dec = dec >> 1;
+  }
+
+  // 取出栈中的数字
+  while (stack.length) {
+    res += stack.pop();
+  }
+
+  // 返回这个字符串
+  return res;
+};
+
+dec2bin(10);
