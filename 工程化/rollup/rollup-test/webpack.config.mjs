1d571a8478ba123420e80c28c2267bdd37e78519
Fan-zexu <1152015013@qq.com>
2024-12-19 22:10:28 +0800

    区分高低版本node实现

--- 工程化/rollup/rollup-test/webpack.config.mjs
+++ 工程化/rollup/rollup-test/webpack.config.mjs
@@ -1,13 +1,29 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 
-/** @type {import("webpack").Configuration} */
+/** @type {import('webpack').Configuration} */
+
 export default {
   entry: "./src/index.js",
-  mode: "development",
-  devtool: false,
   output: {
-    path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist2"),
+    path: path.resolve(import.meta.dirname, "dist2"),
     filename: "bundle.js",
   },
+  devtool: false,
+  mode: "development",
 };
+
+// 低版本Node，这么实现
+
+// import path from "node:path";
+// import { fileURLToPath } from "node:url";
+
+// /** @type {import("webpack").Configuration} */
+// export default {
+//   entry: "./src/index.js",
+//   mode: "development",
+//   devtool: false,
+//   output: {
+//     path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist2"),
+//     filename: "bundle.js",
+//   },
+// };
